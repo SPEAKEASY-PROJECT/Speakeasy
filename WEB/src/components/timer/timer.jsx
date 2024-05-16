c0cd718aa01d2c3c4f0f7fcdefe254cdbb01f055
martab <mbsuarez.92@gmail.com>
2024-05-16 13:43:22 +0200

    feat countdown timer but with bugs in user

--- WEB/src/components/timer/timer.jsx
+++ WEB/src/components/timer/timer.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import AuthContext from "../../contexts/auth.context";
 
 function Timer() {
-    const [timeLeft, setTimeLeft] = useState(30); // 20 minutos en segundos
+    const [timeLeft, setTimeLeft] = useState(30); // 20 minutos en segundos son 1200
     const navigate = useNavigate();
+    // const { user } = useContext(AuthContext);
     
 
     useEffect(() => {
+        // if (!user) {
         const intervalId = setInterval(() => {
             setTimeLeft(prevTime => {
                 if (prevTime <= 1) {
@@ -19,13 +22,19 @@ function Timer() {
         }, 1000);
 
         return () => clearInterval(intervalId);
-    }, [navigate]);
+        // }
+    // }, [navigate, user]);
+}, [navigate]);
 
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
         const remainingSeconds = seconds % 60;
         return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
     };
+
+    // if (user) {
+    //     return null;
+    // }
 
     return <div>{formatTime(timeLeft)}</div>;
 }
